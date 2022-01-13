5f80aaf5be4649e868f66a6779686ce0772c80ef
“Liv <LReinh@PC7918.nordic.local>
2022-01-13 15:00:14 +0100

    work and bank buttons now functional

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 document.getElementById("loan-button").onclick = function() {
-    document.getElementById("loan-balance").innerHTML ="42"
+    document.getElementById("bank-balance").innerHTML ="42"
 }
 
+
+
+//Function for workbutton
+document.getElementById("work-button").onclick = function() {
+   let workBalance=document.getElementById("work-balance").innerHTML
+   document.getElementById("work-balance").innerHTML = parseInt(workBalance,10)+100
+   
+}
+
+//Function for bankbutton
+document.getElementById("bank-button").onclick = function() {
+
+    let bankBalance=document.getElementById("bank-balance").innerHTML
+    let workBalance=document.getElementById("work-balance").innerHTML
+    document.getElementById("bank-balance").innerHTML = parseInt(bankBalance,10)+ parseInt(workBalance,10)
+
+    document.getElementById("work-balance").innerHTML =0
+}
