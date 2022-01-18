481efd02ee70cea54d4e521e2163d86eb83540be
Liv Reinhold <livreinhold@gmail.com>
2022-01-18 15:09:06 +0100

    Added loan repayment functionality

--- app.js
+++ app.js
@@ -1,39 +1,61 @@
+//Function for loan-button
 document.getElementById("loan-button").onclick = function() {
+  let loanBalance=document.getElementById("loan-balance").innerText;
+  let bankBalance=document.getElementById("bank-balance").innerText
 
-  if (document.getElementById("loan-balance").innerText !=0){ alert("You cannot take a loan while you´re still paying off one")
+  //Checks for existing loan
+  if (loanBalance !=0){ alert("You cannot take a loan while you´re still paying off one")
   }else{
   
-  
+  //If no existing loan, number entered in the promt is added to bank and loan balance
       let loanPromt = prompt("How much would you like to loan?");
-     if (Number(loanPromt) > Number(document.getElementById("bank-balance").innerText)*2) {
+     if (Number(loanPromt) > Number(bankBalance)*2) {
           alert("You cannot loan more than double you bank balance")
         }else{
-          alert("You've got a loan")
           document.getElementById("loan-balance").innerText =Number(loanPromt)
           document.getElementById("bank-balance").innerText =Number(document.getElementById("bank-balance").innerText)+Number(loanPromt)
         }
       
       }
-  }
+   //Adds repay-button
+   let repayButton=document.createElement('button')
+   document.getElementById("repay-button").append(repayButton)
+   repayButton.innerText="Repay Loan"
+   repayButton.id="actual-button"
+
+   //Function for repaying
+   document.getElementById("repay-button").onclick = function(){
+    let loanBalance=Number(document.getElementById("loan-balance").innerText)
+    let bankBalance=Number(document.getElementById("bank-balance").innerText)
+
+    document.getElementById("bank-balance").innerText-=loanBalance   
+    document.getElementById("loan-balance").innerText=0
+    document.getElementById("repay-button").remove("repay-button")
+   }
+  } 
   
   
-  
-  //Function for workbutton
+  //Function for work-button
   document.getElementById("work-button").onclick = function() {
      let workBalance=document.getElementById("work-balance").innerText
      document.getElementById("work-balance").innerText = Number(workBalance)+100
      
   }
   
-  //Function for bankbutton
+  //Function for bank-button
   document.getElementById("bank-button").onclick = function() {
-  
+    //Checks for existing loan. If none, balance is added to balance
+    // If a loan exist 10% og work balance goes to loan repayment
+  if(document.getElementById("loan-balance").innerText==0){
       let bankBalance=document.getElementById("bank-balance").innerText
       let workBalance=document.getElementById("work-balance").innerText
       document.getElementById("bank-balance").innerText = Number(bankBalance)+ Number(workBalance)
-  
       document.getElementById("work-balance").innerText =0
-  }
+  }else{
+    document.getElementById("loan-balance").innerText-=Number(document.getElementById("work-balance").innerText)*0.1
+    document.getElementById("bank-balance").innerText=Number(document.getElementById("work-balance").innerText)*0.9+Number(document.getElementById("bank-balance").innerText)
+    document.getElementById("work-balance").innerText=0
+  }}
   
   
   //Fetch and append data to select tag
@@ -57,8 +79,8 @@ document.getElementById("loan-button").onclick = function() {
      console.log(error)
    })
   
+
    //Add information on selected computer to DOM
-  
    document.getElementById("computers").onchange=function(){
      //Gets selected computer
      let selectedvalue = document.getElementById("computers").value
@@ -82,7 +104,8 @@ document.getElementById("loan-button").onclick = function() {
      image.src = `https://noroff-komputer-store-api.herokuapp.com/${selectedComputer[0].image}`;
    }
 
-    //Buys button. Gets the number value of the price for selected computer. 
+
+    // Function for buy-button. Gets the number value of the price for selected computer. 
    document.getElementById("buy-button").onclick = function() {
     let bankBalance=Number(document.getElementById("bank-balance").innerText)
     let computerPrice=Number(document.getElementById("price-computer").innerText)
