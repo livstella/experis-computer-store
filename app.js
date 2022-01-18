300648f6f6aa55589e5f3d82aef0288bea21ee7e
Liv Reinhold <livreinhold@gmail.com>
2022-01-18 14:17:14 +0100

    added the buy function and pictures from API to DOM. minor adjustments for readability. all main functionalities should be implemented.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 document.getElementById("loan-button").onclick = function() {
 
-  if (document.getElementById("loan-balance").innerHTML !=0){ alert("You cannot take a loan while you´re still paying off one")
+  if (document.getElementById("loan-balance").innerText !=0){ alert("You cannot take a loan while you´re still paying off one")
   }else{
   
   
       let loanPromt = prompt("How much would you like to loan?");
-     if (Number(loanPromt) > Number(document.getElementById("bank-balance").innerHTML)*2) {
+     if (Number(loanPromt) > Number(document.getElementById("bank-balance").innerText)*2) {
           alert("You cannot loan more than double you bank balance")
         }else{
           alert("You've got a loan")
-          document.getElementById("loan-balance").innerHTML =Number(loanPromt)
-          document.getElementById("bank-balance").innerHTML =Number(document.getElementById("bank-balance").innerHTML)+Number(loanPromt)
+          document.getElementById("loan-balance").innerText =Number(loanPromt)
+          document.getElementById("bank-balance").innerText =Number(document.getElementById("bank-balance").innerText)+Number(loanPromt)
         }
       
       }
@@ -20,19 +20,19 @@ document.getElementById("loan-button").onclick = function() {
   
   //Function for workbutton
   document.getElementById("work-button").onclick = function() {
-     let workBalance=document.getElementById("work-balance").innerHTML
-     document.getElementById("work-balance").innerHTML = Number(workBalance)+100
+     let workBalance=document.getElementById("work-balance").innerText
+     document.getElementById("work-balance").innerText = Number(workBalance)+100
      
   }
   
   //Function for bankbutton
   document.getElementById("bank-button").onclick = function() {
   
-      let bankBalance=document.getElementById("bank-balance").innerHTML
-      let workBalance=document.getElementById("work-balance").innerHTML
-      document.getElementById("bank-balance").innerHTML = Number(bankBalance)+ Number(workBalance)
+      let bankBalance=document.getElementById("bank-balance").innerText
+      let workBalance=document.getElementById("work-balance").innerText
+      document.getElementById("bank-balance").innerText = Number(bankBalance)+ Number(workBalance)
   
-      document.getElementById("work-balance").innerHTML =0
+      document.getElementById("work-balance").innerText =0
   }
   
   
@@ -76,10 +76,26 @@ document.getElementById("loan-button").onclick = function() {
      currentItem.innerText=item
      document.getElementById("specs-computer").appendChild(currentItem)
      })
+
+     //Sets image-computer scr to the url specified in the api
+     image=document.getElementById('image-computer')
+     image.src = `https://noroff-komputer-store-api.herokuapp.com/${selectedComputer[0].image}`;
    }
 
     //Buys button. Gets the number value of the price for selected computer. 
-   document.getElementById("test-button").onclick = function() {console.log(Number(document.getElementById("price-computer").innerText))}
+   document.getElementById("buy-button").onclick = function() {
+    let bankBalance=Number(document.getElementById("bank-balance").innerText)
+    let computerPrice=Number(document.getElementById("price-computer").innerText)
+
+     if (bankBalance>=computerPrice){
+       alert("You got yourself a new computer")
+       document.getElementById("bank-balance").innerText =0
+     }else(
+       alert("You do not have enough money in the bank")
+     )
+    
+    
+    }
   
    
   
