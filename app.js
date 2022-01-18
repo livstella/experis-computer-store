ba872d2e01d4421bb2fffc2a59093f02517fbd69
Liv Reinhold <livreinhold@gmail.com>
2022-01-18 12:58:23 +0100

    Lost track of changes. But now it fetches data from the api and adds to DOM

--- app.js
+++ app.js
@@ -1,31 +1,85 @@
 document.getElementById("loan-button").onclick = function() {
-    let loanPromt = prompt("How much would you like to loan?");
 
-    if (Number(loanPromt) < Number(document.getElementById("bank-balance").innerHTML)) {
-        alert("You've got a loan")
-        document.getElementById("bank-balance").innerHTML =Number(document.getElementById("bank-balance").innerHTML)+Number(loanPromt)
-      }else{
+  if (document.getElementById("loan-balance").innerHTML !=0){ alert("You cannot take a loan while you´re still paying off one")
+  }else{
+  
+  
+      let loanPromt = prompt("How much would you like to loan?");
+     if (Number(loanPromt) > Number(document.getElementById("bank-balance").innerHTML)*2) {
           alert("You cannot loan more than double you bank balance")
+        }else{
+          alert("You've got a loan")
+          document.getElementById("loan-balance").innerHTML =Number(loanPromt)
+          document.getElementById("bank-balance").innerHTML =Number(document.getElementById("bank-balance").innerHTML)+Number(loanPromt)
+        }
+      
       }
-    
+  }
+  
+  
+  
+  //Function for workbutton
+  document.getElementById("work-button").onclick = function() {
+     let workBalance=document.getElementById("work-balance").innerHTML
+     document.getElementById("work-balance").innerHTML = Number(workBalance)+100
+     
+  }
+  
+  //Function for bankbutton
+  document.getElementById("bank-button").onclick = function() {
+  
+      let bankBalance=document.getElementById("bank-balance").innerHTML
+      let workBalance=document.getElementById("work-balance").innerHTML
+      document.getElementById("bank-balance").innerHTML = Number(bankBalance)+ Number(workBalance)
+  
+      document.getElementById("work-balance").innerHTML =0
+  }
+  
+  
+  //Fetch and append data to select tag
+  let computerArray=[]
+  fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
+   .then(function(response){
+      return response.json()
+   })
+   .then(function(norComputer){
+    for (let computer of norComputer) {
+      computerArray.push(computer)
+    }
+    computerArray.forEach(function(item){
+      let currentItem=document.createElement('option')
+      currentItem.innerText=item.title
+      document.getElementById("computers").appendChild(currentItem)
+    })
+  
+   })
+   .catch(function(error){
+     console.log(error)
+   })
+  
+   //Add information on selected computer to DOM
+  
+   document.getElementById("computers").onchange=function(){
+     //Gets selected computer
+     let selectedvalue = document.getElementById("computers").value
+
+    //Filters computerArray and returns all data for the selected computer
+     const selectedComputer=computerArray.filter(computerArray => computerArray.title==selectedvalue)
+     
+    //Sets ids from html to the data for selected computer
+     document.getElementById("description-computer").innerText=("Description: " + selectedComputer[0].description)
+     document.getElementById("price-computer").innerText=(selectedComputer[0].price)
+
+     //Generates a 'li' element for each spec of the selected computer
+     selectedComputer[0].specs.forEach(function(item){
+     let currentItem=document.createElement('li')
+     currentItem.innerText=item
+     document.getElementById("specs-computer").appendChild(currentItem)
+     })
+   }
+
+    //Buys button. Gets the number value of the price for selected computer. 
+   document.getElementById("test-button").onclick = function() {console.log(Number(document.getElementById("price-computer").innerText))}
+  
    
-}
-
-
-
-//Function for workbutton
-document.getElementById("work-button").onclick = function() {
-   let workBalance=document.getElementById("work-balance").innerHTML
-   document.getElementById("work-balance").innerHTML = Number(workBalance)+100
-   
-}
-
-//Function for bankbutton
-document.getElementById("bank-button").onclick = function() {
-
-    let bankBalance=document.getElementById("bank-balance").innerHTML
-    let workBalance=document.getElementById("work-balance").innerHTML
-    document.getElementById("bank-balance").innerHTML = Number(bankBalance)+ Number(workBalance)
-
-    document.getElementById("work-balance").innerHTML =0
-}
+  
